48903d18a1d1a696e7bc784e77bfbb51126ba148
alvarobgodoy <alvaro.godoy2002@gmail.com>
2022-01-31 18:50:16 -0300

    Added announcement for home page and DOM Manipulation activity

--- chamber/js/script.js
+++ chamber/js/script.js
@@ -4,8 +4,8 @@ window.onload = function() {
         document.getElementById('hamburgerBtn').classList.toggle('open');
     }
 
-    const x = document.getElementById('hamburgerBtn');
-    x.onclick = toggleMenu;
+    const btn = document.getElementById('hamburgerBtn');
+    btn.onclick = toggleMenu;
 
     // Footer
     let lastModified = document.lastModified;
@@ -17,10 +17,22 @@ window.onload = function() {
 
     currentYear.textContent = year;
 
+    // Header date
     const now = new Date();
     const currentDate = new Intl.DateTimeFormat("en-UK", {
         dateStyle: "full"
     }).format(now);
 
     document.getElementById('currentDate').textContent = currentDate;
+
+    function toogleAnnouncement() {
+        document.querySelector('.announcement').classList.toggle('visible');
+    }
+
+    const today = now.getDay()
+    if (today == 1 || today == 2) {
+        document.querySelector('.announcement').classList.toggle('visible');
+        const annBtn = document.getElementById('annBtn');
+        annBtn.onclick = toogleAnnouncement;
+    }
 }
---
+++ list-activity/js/script.js
@@ -0,0 +1,22 @@
+const input = document.querySelector('#favchap');
+const button = document.querySelector('.input button');
+const list = document.querySelector('.list');
+
+button.addEventListener('click', function() {
+    if (input.value !== '') {
+        const liElem = document.createElement('li');
+        const btnEle = document.createElement('button');
+        liElem.textContent = input.value;
+        btnEle.textContent = '❌';
+
+        liElem.appendChild(btnEle);
+        list.appendChild(liElem);
+        btnEle.addEventListener('click', function(event) {
+            const parent = event.target.closest('li');
+            parent.remove();
+            input.value = '';
+            input.focus();
+        });
+    }
+    
+});
