eb3a688409d3220b7de278f8d5e46665507b861c
alvarobgodoy <alvaro.godoy2002@gmail.com>
2022-02-12 17:22:43 -0300

    Week 6 assignment, first version

---
+++ chamber/js/windchill.js
@@ -0,0 +1,12 @@
+let temperature = document.getElementById('temperature').textContent;
+let windSpeed = document.getElementById('wind-speed').textContent;
+let windChill = 'N/A';
+
+console.log(temperature)
+console.log(windSpeed)
+
+if (temperature <= 50 && windSpeed > 3) {
+    windChill = Math.round(35.74 + (0.6215 * temperature) - (35.75 * (windSpeed**0.16)) + (0.4275 * temperature * (windSpeed**0.16)));
+}
+
+document.getElementById('wind-chill').textContent = windChill;
