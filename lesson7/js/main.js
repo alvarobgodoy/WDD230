947b87a37d85584d9e0484910c3c62135ced4114
alvarobgodoy <alvaro.godoy2002@gmail.com>
2022-02-16 16:09:34 -0300

    First version of Progressive loading

---
+++ lesson7/js/main.js
@@ -0,0 +1,31 @@
+const images = document.querySelectorAll('img[data-src]');
+
+const loadImages = (image) => {
+    image.setAttribute('src', image.getAttribute('data-src'));
+    image.onload = () => {
+    image.removeAttribute('data-src');
+    image.style.filter = 'blur(0em)';
+    image.style.transition = 'filter 0.5s';
+    };
+  };
+
+if('IntersectionObserver' in window) {
+    const observer = new IntersectionObserver((items, observer) => {
+      items.forEach((item) => {
+        if(item.isIntersecting) {
+          loadImages(item.target);
+          observer.unobserve(item.target);
+        }
+      });
+    });
+    images.forEach((img) => {
+      observer.observe(img);
+    });
+  } else {
+    images.forEach((img) => {
+      loadImages(img);
+    });
+  }
+
+  // filter: blur(0em);
+  // transition: filter 0.5s;
